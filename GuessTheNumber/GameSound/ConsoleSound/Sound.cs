ce3c726088d512f912109b1ebc36f2d6f7bf504c
Федор Шарапов <sharapovfedor@yandex.ru>
2022-09-22 23:03:42 +0300

    добавлено проигрование звуков в игре

---
+++ GuessTheNumber/GameSound/ConsoleSound/Sound.cs
@@ -0,0 +1,16 @@
+﻿namespace GuessTheNumber.GameSound.ConsoleSound
+{
+    class Sound
+    {
+        public int Frequency { get; }
+        public int Duration { get; }
+        public int Pause { get; }
+
+        public Sound(int frequency, int duration, int pause)
+        {
+            Frequency = frequency;
+            Duration = duration;
+            Pause = pause;
+        }
+    }
+}
---
+++ GuessTheNumber/GameSound/ConsoleSound/SoundTrack.cs
@@ -0,0 +1,14 @@
+﻿namespace GuessTheNumber.GameSound.ConsoleSound
+{
+    class SoundTrack
+    {
+        public string Name { get; }
+        public Sound[] Track { get; }
+
+        public SoundTrack(string name, Sound[] track)
+        {
+            Name = name;
+            Track = track;
+        }
+    }
+}
