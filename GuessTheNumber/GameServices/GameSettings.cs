f43cfff41966bd046428ce7b0211764536d03e4d
Федор Шарапов <sharapovfedor@yandex.ru>
2022-09-22 23:02:11 +0300

    добавлены настройки игры

---
+++ GuessTheNumber/GameServices/GameSettings.cs
@@ -0,0 +1,23 @@
+﻿using GuessTheNumber.Games.Interfaces;
+
+namespace GuessTheNumber.Games
+{
+    class GameSettings
+    {
+        /// <summary>
+        /// Нижняя граница диапазона загадываемого числа
+        /// </summary>
+        /// 
+        public int SecretNumberMin { get; set; }
+
+        /// <summary>
+        /// Верхняя граница диапазона загадываемого числа
+        /// </summary>
+        public int SecretNumberMax { get; set; }
+
+        /// <summary>
+        /// Количество попыток, за каторое необходимо угадать загаданное число
+        /// </summary>
+        public int AttemptMax { get; set; }
+    }
+}
