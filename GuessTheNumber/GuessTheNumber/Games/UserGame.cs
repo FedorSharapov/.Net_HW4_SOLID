0975d24d11623e20777497b61b4b13ab3a31cbe4
Федор Шарапов <sharapovfedor@yandex.ru>
2022-09-27 11:43:23 +0300

    Исправлена ошибка изменения настроек пользовательской игры, после завершения основной игры.

--- GuessTheNumber/GuessTheNumber/Games/UserGame.cs
+++ GuessTheNumber/GuessTheNumber/Games/UserGame.cs
@@ -1,24 +1,28 @@
 ﻿using GuessTheNumber.Games.Interfaces;
 using GuessTheNumber.Levels.Interfaces;
+using GuessTheNumber.Settings;
 
 namespace GuessTheNumber.Services
 {
     public class UserGame : BaseGame, IUserGame
     {
+        private ILevelSettings _userSettings;
         IUserLevelInit _levelInit;
 
         public UserGame(ILevel level, IUserLevelInit levelInit) : base(level)
         {
             _levelInit = levelInit;
+            _userSettings = new LevelSettings();
         }
 
         public void Init()
         {
-            _level.Settings = _levelInit.Init();
+            _userSettings = _levelInit.Init();
         }
 
         public override void Play()
         {
+            _level.Settings = _userSettings;
             _level.Start();
         }
     }
